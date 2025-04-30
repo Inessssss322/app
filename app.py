c7517349c88715665315725958826fadf93d0f28
Inessssss322 <inesaanovska11@gmail.com>
2025-04-30 10:40:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ import streamlit as st
 
 st.title('Перевірка додатку')
 name = st.text_input("Для перевірки напишіть слово 'name'")
-if name:
-    st.success(f"Первірка пройдена")
-else:
-    st.error(f"Перевірка не пройдена")
+answer = "name"
+
+if:
+    if name:
+        st.success(f"Первірка пройдена")
+    else:
+        st.error(f"Перевірка не пройдена")
