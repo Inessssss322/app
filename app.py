55249a3dc41dc4e9f6094aed6384ed43eb179347
Inessssss322 <inesaanovska11@gmail.com>
2025-04-30 10:43:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ st.title('Перевірка додатку')
 name = st.text_input("Для перевірки напишіть слово 'name'")
 answer = "name"
 
-if final:
+if st.text_input:
     if name == answer:
         st.success(f"Первірка пройдена")
     else:
